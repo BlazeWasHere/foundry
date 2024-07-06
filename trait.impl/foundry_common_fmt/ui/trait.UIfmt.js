58cdddb245bfdf40b9309508cfef03016cab637b
BlazeWasHere <BlazeWasHere@users.noreply.github.com>
2024-07-06 00:55:09 +0000

    deploy: ce38f5328963e30d2242a87fed7b0e49fcbf84b1

---
+++ trait.impl/foundry_common_fmt/ui/trait.UIfmt.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {
+"foundry_common":[["impl <a class=\"trait\" href=\"foundry_common_fmt/ui/trait.UIfmt.html\" title=\"trait foundry_common_fmt::ui::UIfmt\">UIfmt</a> for <a class=\"struct\" href=\"foundry_common/transactions/struct.TransactionReceiptWithRevertReason.html\" title=\"struct foundry_common::transactions::TransactionReceiptWithRevertReason\">TransactionReceiptWithRevertReason</a>"]],
+"foundry_common_fmt":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
